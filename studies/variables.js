e13b9e23c55764d32ecba56ad5a5125c57eb8449
Ubuntu <ubuntu@ip-172-31-5-165.ec2.internal>
2021-06-26 23:36:23 +0000

    Updating variables.js

--- studies/variables.js
+++ studies/variables.js
@@ -1,22 +1,26 @@
 // VARIABLES:
 /* Purpose
- * 0: We use variables in JavaScript to hold items in memory throughout the duration of a program.
- *	The term 'variable' refers to their very nature; once they are created; their assigned value
-	or the type of value to which they point may change. As identifiers, variables point to
-	values of a specific type; including: Number, String, Boolean, Array, Object, or other data
-	types.
+ * 0: We use variables in JavaScript to hold items in memory throughout the duration of a
+    program.
+ *	The term 'variable' refers to their very nature; once they are created; their assigned
+    value or the type of value to which they point may change. As identifiers, variables
+    point to values of a specific type; including: Number, String, Boolean, Array,
+    Object, or other data types.
 */
  
 /* Use
- * 0: To create a variable we use the keyword, var, followed by our desired variable name. It is best
-	practice to give your variable a name that reflects it's value.
-		Example: if we wanted to assign a variable to a person's first name; we might use:
-		var firstName = "Ron"
- * 1: Did you notice that firstName is all one word and oddly capitalized? Variable names should be one
-	word, and named in what's referred to as camelCase convention. When combining two or more words
-	to formulate a variable name, the first word or letter is lower-cased and all remaining words
-	have their first letter capitalized. Examples: fullName, dayOfMonth, array, aNewArray	
- * 2. There are two phases of using variables: declaration and initialization (or assignment).
+ * 0: To create a variable we use the keyword, var, followed by our desired
+    variable name. It is best practice to give your variable a name that reflects
+    it's value.
+		Example: if we wanted to assign a variable to a person's first name,
+		we might use: var firstName = "Ron"
+ * 1: Did you notice that firstName is all one word and oddly capitalized? Variable
+    names should be one word, and named in what's referred to as camelCase
+    convention. When combining two or more words to formulate a variable name, the
+    first word or letter is lower-cased and all remaining words	have their first
+    letter capitalized. Examples: fullName, dayOfMonth, array, aNewArray	
+ * 2. There are two phases of using variables: declaration and initialization
+    (or assignment).
 */
 // 1. declaration //
 var myName;
@@ -35,7 +39,7 @@ console.log(myName); // prints => john
 myName = 'bob';
 console.log(myName); // prints => bob
 
-// NOTE: We can assign and re-assign anything to a variable - we cannot do this with constants //
+// NOTE: We can assign and re-assign anything to a variable-we can't do this with constants //
 var myVariable = 1;
 var myVariable = true;
 myVariable = "someString";
