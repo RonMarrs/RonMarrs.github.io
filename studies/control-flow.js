28a9683a295e19fa5fca1abb96c1f9155494f56b
Ubuntu <ubuntu@ip-172-31-5-165.ec2.internal>
2021-06-30 00:00:42 +0000

    Uploading control-loop.js 29 June 21

---
+++ studies/control-flow.js
@@ -0,0 +1,17 @@
+// CONTROL FLOW:
+/* Purpose
+  0: Using looping and/or conditional statements, our code can make decisions and 
+	determine the next course of action. Or our code can repeat a piece of code
+	over and over in order to loop over values. The use of control flow statements 
+	creates interactivity with the application. 
+*/
+
+/* Types
+  0: { } Block - used to group statement together
+  1: Break – stops a statement or a loop when a condition has been met to prevent
+	 an endless loop
+  2: Continue – jumps a loop from one iteration to the next when a condition
+	 has been met
+  3: If Statement – executes a statement if the condition that is specified is truthy
+  4: Switch Statement – used when there are numerous if statements for one function.
+*/
